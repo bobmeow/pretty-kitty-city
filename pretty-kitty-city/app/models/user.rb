420c5d5a35ac92a106668235dcc477b9876cc3bf
Vinny Martinez <vinny@vinnymartinez.com>
2017-01-09 18:00:18 -0500

    created view-forms for user

--- pretty-kitty-city/app/models/user.rb
+++ pretty-kitty-city/app/models/user.rb
@@ -2,4 +2,5 @@ class User < ApplicationRecord
   has_one :pet, :foreign_key => 'owner_id'
   has_many :friendships
   has_many :pet_friends, through: :friendships, source: :pet
+  has_secure_password
 end
