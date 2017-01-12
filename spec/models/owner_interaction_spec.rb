f0e9de5abca91e1d5355f2f11f19ffa669f8a84c
github username <github email address>
2017-01-12 06:27:57 -0500

    added two specs

--- spec/models/owner_interaction_spec.rb
+++ spec/models/owner_interaction_spec.rb
@@ -1,5 +1,19 @@
 require 'rails_helper'
 
-RSpec.describe OwnerInteraction, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+describe OwnerInteraction do
+
+  let(:user) {User.create(name: "Josh", password: "123")}
+  let(:valid_owner) {OwnerInteraction.create(owner_id: user.id, kind: "feed")}
+  let(:valid_kind) {OwnerInteraction.create(owner_id: user.id, kind: "feed")}
+  
+  it "has an owner" do
+    expect(valid_owner).to be_valid
+  end
+
+  it "has an owner action kind" do
+    expect(valid_kind).to be_valid
+  end
+
+
+  
 end
